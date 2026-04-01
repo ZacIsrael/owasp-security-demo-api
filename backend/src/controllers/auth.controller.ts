b10803a958a557f0987dbacef94325eaa52d2610
Zac Israel <zac390@gmail.com>
2026-04-01 14:12:35 -0400

    feat(auth): implement login controller with credential validation and JWT cookie authentication

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -13,6 +13,10 @@ import dotenv from "dotenv";
 import path from "node:path";
 import { asyncHandler } from "../middleware/async.middleware";
 import { userService } from "../services/user.service";
+import { ErrorResponse } from "../utils/errorResponse";
+import { comparePassword } from "../utils/helpers";
+import { generateJwt } from "../utils/auth.util";
+import { sendTokenResponse } from "../utils/auth-response.util";
 
 export const register = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -42,7 +46,39 @@ export const register = asyncHandler(
   }
 );
 
-//
+// Logs a user in
 const login = asyncHandler(
-  async (reg: Request, res: Response, next: NextFunction) => {}
+  async (req: Request, res: Response, next: NextFunction) => {
+    // req.body has already been validated and normalized by validateBody(LoginDTO)
+    const { email, password } = req.body;
+
+    // req.body has already been validated and sanitized by
+    // validateBody(LoginDTO) in auth.route.ts.
+
+    // To show the vulnerability, I'll just comment out
+    // validateBody(LoginDTO) in that particular route.
+
+    // look up user by email
+    const { user } = await userService.getUserByEmail(email);
+
+    // Invalid credentials (best practice not to reveal whether email exists)
+    // Could be a 401 or 404...for now, I'll leave it as 401
+    if (user === null) {
+      throw new ErrorResponse("Invalid credentials", 401);
+    }
+
+    // Check if the password matches
+    const isMatch = await comparePassword(password, user.password_hash);
+
+    // Throw error if the password does not match
+    if (!isMatch) {
+      throw new ErrorResponse("Invalid credentials", 401);
+    }
+
+    // Client entered valid login credentials so generate jwt token using user id
+    const token = generateJwt(user.id);
+
+    // call sendTokenReponse
+    sendTokenResponse(user, token, 200, res);
+  }
 );
