c9e5d8dee2c9af317b1419f2d412485112104164
Zac Israel <zac390@gmail.com>
2026-04-01 14:39:40 -0400

    feat(auth): add auth routes with toggleable validation for vulnerability and security demonstrations

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,25 @@
+// Import the Express framework for building HTTP servers
+import express from "express";
+
+import { register, login, logout } from "../controllers/auth.controller.js";
+
+import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto.js";
+
+import { validateBody } from "../middleware/validate.middleware.js";
+
+const router = express.Router();
+
+// Register a user
+// Vulnerable
+router.post("/register", register);
+// router.post("/register", validateBody(CreateUserDTO), register);
+
+// User can login
+// Vulnerable
+router.post("/login", login);
+// router.post("/login", validateBody(LoginUserDTO), login);
+
+// Log a user out
+router.post("/logout", logout);
+
+export default router;
